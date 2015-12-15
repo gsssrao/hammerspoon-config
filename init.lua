b237087f422a02598809b1c99670ce39360de529
Srinivas <sairao1996@gmail.com>
2015-12-15 15:26:55 +0530

    Added Mouse and other functions

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+local mouseCircle = nil
+local mouseCircleTimer = nil
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
@@ -72,43 +75,100 @@ hs.hotkey.bind({"cmd", "ctrl"}, "C", function()
   win:setFrame(f)
 end)
 
+
 local cmdalt = {"cmd", "alt"}
 local hyper = {"cmd","alt","ctrl"}
 
+-- Windows Hints
 hs.hotkey.bind(hyper, "i", function()
     hs.hints.windowHints()
+    local win = hs.window.focusedWindow()
+    local max = win:frame()
+    local center = hs.geometry.rectMidPoint(max)
+
+    hs.mouse.setAbsolutePosition(center)
 end)
 
 hs.hotkey.bind(cmdalt, 'Up', function()
     hs.window.focusedWindow():focusWindowNorth()
+    local win = hs.window.focusedWindow()
+    local max = win:frame()
+    local center = hs.geometry.rectMidPoint(max)
+
+    hs.mouse.setAbsolutePosition(center)
 end)
 
 hs.hotkey.bind(cmdalt, 'Down', function()
     hs.window.focusedWindow():focusWindowSouth()
+    local win = hs.window.focusedWindow()
+    local max = win:frame()
+    local center = hs.geometry.rectMidPoint(max)
+
+    hs.mouse.setAbsolutePosition(center)
 end)
 
 hs.hotkey.bind(cmdalt, 'Right', function()
     hs.window.focusedWindow():focusWindowEast()
+    local win = hs.window.focusedWindow()
+    local max = win:frame()
+    local center = hs.geometry.rectMidPoint(max)
+
+    hs.mouse.setAbsolutePosition(center)
 end)
 
 hs.hotkey.bind(cmdalt, 'Left', function()
     hs.window.focusedWindow():focusWindowWest()
+    local win = hs.window.focusedWindow()
+    local max = win:frame()
+    local center = hs.geometry.rectMidPoint(max)
+
+    hs.mouse.setAbsolutePosition(center)
+
 end)
 
-local caffeine = hs.menubar.new()
-function setCaffeineDisplay(state)
-    if state then
-        caffeine:setTitle("AWAKE")
-    else
-        caffeine:setTitle("SLEEPY")
+-- FullScreen Binding
+hs.hotkey.bind({"cmd","ctrl"}, "f", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+
+end)
+
+-- Wifi Binding
+
+-- Ethernet Binding
+
+-- Mouse Highlight
+
+function mouseHighlight()
+    local win = hs.window.focusedWindow()
+    local max = win:frame()
+    local center = hs.geometry.rectMidPoint(max)
+
+    hs.mouse.setAbsolutePosition(center)
+    
+    if mouseCircle then
+        mouseCircle:delete()
+        if mouseCircleTimer then
+            mouseCircleTimer:stop()
+        end
     end
+    mousepoint = hs.mouse.getAbsolutePosition()
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    mouseCircle:setFill(false)
+    mouseCircle:setStrokeWidth(5)
+    mouseCircle:bringToFront(true)
+    mouseCircle:show()
+
+    mouseCircleTimer = hs.timer.doAfter(3, function() mouseCircle:delete() end)
 end
 
-function caffeineClicked()
-    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
-end
-
-if caffeine then
-    caffeine:setClickCallback(caffeineClicked)
-    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
-end
+hs.hotkey.bind(hyper, 'm', mouseHighlight)
